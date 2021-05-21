396bb2dcc160d83ddf8110248014edd491d19d55
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-05-21 17:31:20 +0300

    create auth routes register

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -15,7 +15,29 @@ router.post('/register',
     ],
     async (req, res) => {
         try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    message: 'Некорректные данные при регистрации',
+                    // errors: errors.array()
+                })
+            }
 
+            const {email, password} = req.body
+
+            const candidate = await User.findOne({email})
+            if (candidate) {
+                return res.status(400).json({message: 'Такой пользователь уже существует'})
+            }
+
+            const hashPassword = await bcrypt.hash(password, 12)
+
+            const user = new User({email, password: hashPassword})
+            await user.save()
+
+            await fileServices.createDir(new File({name: '', user: user.id}))
+
+            return res.json({message: 'Пользователь зарегистрирован'})
         } catch (e) {
             console.log(e.message)
             return res.status(400).json({message: 'Что-то пошло не так'})
