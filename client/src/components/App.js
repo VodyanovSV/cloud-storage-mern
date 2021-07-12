7746640037173be1c4a705827f1f7022a54249dd
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-07-12 18:36:36 +0300

    create routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,18 @@
 import './app.scss'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
 
     return (
-
+        <BrowserRouter>
+            <div className="app">
+                <div className="wrap">
+                    <Routes>
+                        <Route path={'/registration'} element={<Registration/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
