84b162ecad207ad527e4c0f426169d3186ec024e
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-07-26 17:32:20 +0300

    create logout on navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -3,9 +3,11 @@ import {Link, NavLink} from 'react-router-dom'
 import './navbar.scss'
 import Logo from '../../assets/images/navbar-logo.svg'
 import {useDispatch, useSelector} from "react-redux";
+import {logoutUserActionCreator} from "../../store/actionCreators/userActionCreators";
 
 const Navbar = () => {
     const isAuth = useSelector(state => state.userReduser.isAuth)
+    const dispatch = useDispatch()
 
     return (
         <div className='navbar'>
@@ -19,6 +21,10 @@ const Navbar = () => {
                 {
                     !isAuth &&
                     <div className={'navbar__registration'}><NavLink to={'/registration'}>Регистрация</NavLink></div>
+                }
+                {
+                    isAuth &&
+                    <div className={'navbar__login'} onClick={() => dispatch(logoutUserActionCreator())}>Выйти</div>
                 }
             </div>
         </div>
