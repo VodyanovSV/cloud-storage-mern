d6e283a14b9f91deaa7fb94588efb9bd16542a63
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-08-27 18:33:36 +0300

    create component uploadfile

--- client/src/components/pages/disk/uploader/uploadFile/UploadFile.js
+++ client/src/components/pages/disk/uploader/uploadFile/UploadFile.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import styles from './UploadFile.module.scss'
+import {useDispatch} from "react-redux";
+import {removeFileUploaderActionCreator} from "../../../../../store/actionCreators/uploaderActionCreators";
 
 const UploadFile = ({file}) => {
 
-    return (
+    const dispatch = useDispatch()
 
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <p className={styles.title}>
+                    {file.name}
+                </p>
+                <button className={styles.close} onClick={()=>dispatch(removeFileUploaderActionCreator(file))}>
+                    X
+                </button>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.progress} style={{width: file.progress + '%'}}></div>
+                <p className={styles.percent}>{file.progress}%</p>
+            </div>
+        </div>
     );
 };
 
