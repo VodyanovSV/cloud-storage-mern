f3f749d4b6d7c5f56016fea5c8878d7929b2b81c
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-08-30 17:21:17 +0300

    create component uploader

--- client/src/components/pages/disk/uploader/Uploader.js
+++ client/src/components/pages/disk/uploader/Uploader.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import styles from './Uploader.module.scss'
+import UploadFile from "./uploadFile/UploadFile";
+import {useDispatch, useSelector} from "react-redux";
+import {uploaderHideActionCreator} from "../../../../store/actionCreators/uploaderActionCreators";
+
 
 const Uploader = () => {
 
-    return (isVisible &&
+    const isVisible = useSelector((state) => state.uploaderReduser.isVisible)
+    const dispatch = useDispatch()
+    const files = useSelector((state) => state.uploaderReduser.files)
 
+    return (isVisible &&
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <p className={styles.title}>
+                    Загрузки
+                </p>
+                <button className={styles.close} onClick={() => {
+                    dispatch(uploaderHideActionCreator())
+                }}>
+                    X
+                </button>
+            </div>
+            <div className={styles.content}>
+                {files.map(file => <UploadFile key={file.id} file={file}/>)}
+            </div>
+        </div>
     );
 };
 
