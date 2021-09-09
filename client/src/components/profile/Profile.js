d47a380d129b1ed0f6b2ba4a28b1815eebd70732
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-09-09 20:54:09 +0300

    create component profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {useDispatch} from "react-redux";
+import {deleteAvatar, uploadAvatar} from "../../actions/avatarUser";
 
 const Profile = () => {
 
-    return (
+    const dispatch = useDispatch()
 
+    function avatarUploadHandler(event) {
+        const file = event.target.files[0]
+        dispatch(uploadAvatar(file))
+    }
+
+    function avatarDeleteHandler() {
+        dispatch(deleteAvatar())
+    }
+
+    return (
+        <div>
+            <button onClick={avatarDeleteHandler}>Удалить аватар</button>
+            <input accept={'image/*'} type='file' placeholder='Загрузить аватар' onChange={avatarUploadHandler}/>
+        </div>
     );
 };
 
