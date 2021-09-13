35a032783ecbf4023c350ec80c063370ca859732
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-09-13 17:37:39 +0300

    create page login

--- client/src/components/pages/autorization/Login.js
+++ client/src/components/pages/autorization/Login.js
@@ -1,10 +1,26 @@
 import React, {useState} from 'react';
 import './autorization.scss'
+import Input from "../../UI/input/Input";
+import {loginUser} from '../../../actions/loginUser'
+import {useDispatch} from "react-redux";
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
 
     return (
-
+        <div className={'autorization'}>
+            <div className={'autorization__header'}>Авторизация</div>
+            <Input type={'text'} placeholder={'Введите email'} value={email} setValue={setEmail}/>
+            <Input type={'password'} placeholder={'Введите пароль'} value={password} setValue={setPassword}/>
+            <button
+                className={'autorization__btn'}
+                onClick={()=>dispatch(loginUser(email, password))}
+            >
+                Войти
+            </button>
+        </div>
     );
 };
 
