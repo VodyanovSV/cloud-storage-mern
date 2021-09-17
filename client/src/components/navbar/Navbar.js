9aa4007698d77a5565d80c0aeed4999df6937544
Sergey Vodyanov <VodyanovSV@gmail.com>
2021-09-17 20:40:48 +0300

    finished the navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,10 +4,39 @@ import './navbar.scss'
 import Logo from '../../assets/images/navbar-logo.svg'
 import {useDispatch, useSelector} from "react-redux";
 import {logoutUserActionCreator} from "../../store/actionCreators/userActionCreators";
+import {getFile, searchFile} from "../../actions/file";
+import defaultAvatar from '../../assets/images/avatar.png'
+import {API_URL} from "../../config";
 
 const Navbar = () => {
     const isAuth = useSelector(state => state.userReduser.isAuth)
+    const currentUser = useSelector(state => state.userReduser.currentUser)
+    const currentDir = useSelector(state => state.fileReduser.currentDir)
     const dispatch = useDispatch()
+    const [searchName, setSearchName] = useState('')
+    const [searchTimeout, setSearchTimeout] = useState(false)
+    const avatar = currentUser.avatar ? API_URL + currentUser.avatar : defaultAvatar
+
+    function changeInputHandler(event) {
+
+        setSearchName(event.target.value)
+
+        if (searchTimeout) {
+            clearTimeout(searchTimeout)
+        }
+
+        setSearchTimeout(
+            setTimeout((value) => {
+                if (value !== '') {
+                    dispatch(searchFile(value))
+                } else {
+                    dispatch(getFile(currentDir, 'type'))
+                }
+            }, 500, event.target.value)
+        )
+
+
+    }
 
     return (
         <div className='navbar'>
@@ -24,8 +53,22 @@ const Navbar = () => {
                 }
                 {
                     isAuth &&
+                    <div className="input-field col s6 navbar__search">
+                        <input placeholder="Поиск" type="text" className="validate" onChange={changeInputHandler}
+                               value={searchName}/>
+                    </div>
+                }
+                {
+                    isAuth &&
                     <div className={'navbar__login'} onClick={() => dispatch(logoutUserActionCreator())}>Выйти</div>
                 }
+                {
+                    isAuth &&
+                    <Link to={'/profile'}>
+                        <img className={'navbar__avatar'} src={avatar} alt=""/>
+                    </Link>
+                }
+
             </div>
         </div>
     );
